ecb08dde2efa197659714c0bc4c678fed99cb416
lbalbrecht <lalbrecht18@wooster.edu>
2020-12-18 10:52:11 -0800

    NYT url second attempt

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
 
     //TODO everything
 
+    var searchArticle = 
+    var getArticle = $.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}&fq={filter}&api-key=WHXnuxQs7RfnLtpI2LqFhYggpqiMShcJ`)
+    
     
 });
